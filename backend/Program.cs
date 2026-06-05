d93b0f520d98b514dd3a4c60f3b503d286843964
namingi <125250519+namingi@users.noreply.github.com>
2026-06-05 14:42:21 +0900

    Update Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -4,6 +4,11 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls(
+"http://0.0.0.0:8080"
+);
+
+builder.Services.AddControllers();
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
